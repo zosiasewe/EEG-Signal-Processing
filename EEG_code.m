294324daeb60d6fb8006b89a8e8d327e63fc31c8
zosiasewe <zosiasewe2@gmail.com>
2025-07-24 11:33:48 +0300

    eeg lab

--- EEG_code.m
+++ EEG_code.m
@@ -2,6 +2,8 @@ clc
 clear all 
 close all
 
+% Data preparation
+
 closed_path = fullfile(pwd, 'closed_nose', 'AC.mat');
 % lalala
 tmp = load(closed_path);
@@ -11,22 +13,14 @@ data_perm = permute(data, [3, 2, 1]);  % → [channels × time × trials] = [17 
 EEG_data = reshape(data_perm, 17, []);  % → [17 × (1280 * 160)] = [17 × 204800]
 % 1280 punktów czasowych/trial × 160 trials = 204800 próbek na kanał
 
-
-
 eeglab;
 
-
-
 %% Continuos data
-%pop_importdata - we say the num of channels, data , sampling rate, points,
-%xmin the rest it does on its own
 EEG_continuous = pop_importdata('dataformat','array','nbchan', 17, 'data', EEG_data,'srate', 256,'pnts', size(EEG_data,2),'xmin', 0);
-
 EEG_continuous = eeg_checkset(EEG_continuous);
-[ALLEEG, EEG_continuous, Current_set] = eeg_store(ALLEEG, EEG_continuous, 1);
+[ALLEEG, EEG_continuous, CURRENTSET] = eeg_store(ALLEEG, EEG_continuous, 1);
 
-pop_eegplot(EEG_continuous);
-
+pop_eegplot(EEG_continuous, 1, 1, 1);
 
 %% Data per epochs
 EEG_epoched = eeg_emptyset();
@@ -40,7 +34,10 @@ EEG_epoched.xmax = (1280-1)/256;
 EEG_epoched.times = (0:1279) * 1000/256; % time vector in ms [0, 3.906, 7.812, ...]
 EEG_epoched = eeg_checkset(EEG_epoched);
 
-[ALLEEG, EEG_epoched, Current_set] = eeg_store(ALLEEG, EEG_epoched, 2);
+[ALLEEG, EEG_epoched, CURRENTSET] = eeg_store(ALLEEG, EEG_epoched, 2);
 
-pop_eegplot(EEG_epoched);
+pop_eegplot(EEG_epoched, 1, 1, 1);
+
+
+
 
